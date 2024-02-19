6336a258d44fbcb5e42eb9163e995a2c66e00b53
HEEEUN9812 <heeeun9812@gmail.com>
2024-02-20 02:01:37 +0900

    Error 메세지 관리

--- src/main/java/com/sparta/myselectshop/service/ProductService.java
+++ src/main/java/com/sparta/myselectshop/service/ProductService.java
@@ -4,11 +4,14 @@ import com.sparta.myselectshop.dto.ProductMypriceRequestDto;
 import com.sparta.myselectshop.dto.ProductRequestDto;
 import com.sparta.myselectshop.dto.ProductResponseDto;
 import com.sparta.myselectshop.entity.*;
+import com.sparta.myselectshop.exception.ProductNotFoundException;
 import com.sparta.myselectshop.naver.dto.ItemDto;
 import com.sparta.myselectshop.repository.FolderRepository;
 import com.sparta.myselectshop.repository.ProductFolderRepository;
 import com.sparta.myselectshop.repository.ProductRepository;
+import java.util.Locale;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.MessageSource;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -27,6 +30,7 @@ public class ProductService {
     private final ProductRepository productRepository;
     private final FolderRepository folderRepository;
     private final ProductFolderRepository productFolderRepository;
+    private final MessageSource messageSource;
 
     public static final int MIN_MY_PRICE = 100;
 
@@ -39,17 +43,30 @@ public class ProductService {
     public ProductResponseDto updateProduct(Long id, ProductMypriceRequestDto requestDto) {
         int myprice = requestDto.getMyprice();
         if (myprice < MIN_MY_PRICE) {
-            throw new IllegalArgumentException("유효하지 않은 관심 가격입니다. 최소 " + MIN_MY_PRICE + "원 이상으로 설정해주세요.");
+            throw new IllegalArgumentException(
+                messageSource.getMessage(
+                    "below.min.my.price",
+                    new Integer[]{MIN_MY_PRICE},
+                    "Wrong Price",
+                    Locale.getDefault()
+                )
+            );
         }
         Product product = productRepository.findById(id).orElseThrow(() ->
-                new NullPointerException("해당 상품을 찾을 수 없습니다.")
+            new ProductNotFoundException(messageSource.getMessage(
+                "not.found.product",
+                null,
+                "Not Found Product",
+                Locale.getDefault()
+            ))
         );
         product.update(requestDto);
         return new ProductResponseDto(product);
     }
 
     @Transactional(readOnly = true)
-    public Page<ProductResponseDto> getProducts(User user, int page, int size, String sortBy, boolean isAsc) {
+    public Page<ProductResponseDto> getProducts(User user, int page, int size, String sortBy,
+        boolean isAsc) {
 
         Sort.Direction direction = isAsc ? Sort.Direction.ASC : Sort.Direction.DESC;
         Sort sort = Sort.by(direction, sortBy);
@@ -70,7 +87,7 @@ public class ProductService {
     @Transactional
     public void updateBySearch(Long id, ItemDto itemDto) {
         Product product = productRepository.findById(id).orElseThrow(() ->
-                new NullPointerException("해당 상품은 존재하지 않습니다.")
+            new NullPointerException("해당 상품은 존재하지 않습니다.")
         );
         product.updateByItemDto(itemDto);
     }
@@ -78,34 +95,37 @@ public class ProductService {
     public void addFolder(Long productId, Long folderId, User user) {
 
         Product product = productRepository.findById(productId).orElseThrow(
-                ()-> new NullPointerException("해당 상품이 존재하지 않습니다.")
+            () -> new NullPointerException("해당 상품이 존재하지 않습니다.")
         );
 
-        Folder folder = folderRepository. findById(folderId).orElseThrow(
-                () -> new NullPointerException("해당 폴더가 존재하지 않습니다.")
+        Folder folder = folderRepository.findById(folderId).orElseThrow(
+            () -> new NullPointerException("해당 폴더가 존재하지 않습니다.")
         );
 
-        if(!product.getUser().getId().equals(user.getId())
-        || !folder.getUser().getId().equals(user.getId())){
+        if (!product.getUser().getId().equals(user.getId())
+            || !folder.getUser().getId().equals(user.getId())) {
             throw new IllegalArgumentException("회원님의 관심상품이 아니거나, 회원님의 폴더가 아닙니다.");
         }
 
-        Optional<ProductFolder> overlapFolder = productFolderRepository.findByProductAndFolder(product, folder);
+        Optional<ProductFolder> overlapFolder = productFolderRepository.findByProductAndFolder(
+            product, folder);
 
-        if(overlapFolder.isPresent()){
-            throw  new IllegalArgumentException("중복된 폴더입니다.");
+        if (overlapFolder.isPresent()) {
+            throw new IllegalArgumentException("중복된 폴더입니다.");
         }
 
         productFolderRepository.save(new ProductFolder(product, folder));
     }
 
-    public Page<ProductResponseDto> getProductsInFolder(Long foldersId, int page, int size, String sortBy, boolean isAsc, User user) {
+    public Page<ProductResponseDto> getProductsInFolder(Long foldersId, int page, int size,
+        String sortBy, boolean isAsc, User user) {
 
         Sort.Direction direction = isAsc ? Sort.Direction.ASC : Sort.Direction.DESC;
         Sort sort = Sort.by(direction, sortBy);
         Pageable pageable = PageRequest.of(page, size, sort);
 
-        Page<Product> productList = productRepository.findAllByUserAndProductFolderList_FolderId(user, foldersId, pageable);
+        Page<Product> productList = productRepository.findAllByUserAndProductFolderList_FolderId(
+            user, foldersId, pageable);
 
         Page<ProductResponseDto> responseDtoList = productList.map(ProductResponseDto::new);
 
